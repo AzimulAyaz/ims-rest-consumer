0d32a21001960e70b48c55b36b59d2f7bb7cef5f
Azimul Ayaz <azimulayaz4616@gmail.com>
2022-08-01 16:38:55 +0530

    Final Commit 1.0

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule} from '@angular/common/http';
+
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule,
+        
       ],
       declarations: [
         AppComponent
@@ -20,16 +25,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ims-rest-consumer'`, () => {
+  it(`should have as title 'Product Inventory Management System'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ims-rest-consumer');
+    expect(app.title).toEqual('Product Inventory Management System');
   });
 
-  it('should render title', () => {
+  it(`should render title in h2 tag`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ims-rest-consumer app is running!');
+    const compiled=fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Product Inventory Management System');
   });
+
 });
